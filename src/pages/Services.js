4267125540cf2c9975f4372a9426b6b83f3547a2
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-05-16 18:54:06 +0600

    navbar done

---
+++ src/pages/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h3>All our services here.</h3>
+        </div>
+    );
+};
+
+export default Services;
